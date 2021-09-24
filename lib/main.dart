80b0b45a09fffeab4e8eb0eafdc6108feeba734b
Daniel Bielech <daniel.bielech@ideaworks.co.uk>
2021-09-24 16:20:04 +0100

    Implemented bottom menu bar with 4 button in a row and one floating button

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Sketching App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepOrange,
       ),
       home: const MainPage(),
     );
@@ -30,6 +31,30 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: const Center(
+        child: Text('Canvas'),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        child: SizedBox(
+          height: 100.0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(onPressed: () => {}, icon: const Icon(Icons.menu)),
+              IconButton(onPressed: () => {}, icon: const Icon(Icons.face)),
+              IconButton(onPressed: () => {}, icon: const Icon(Icons.nat)),
+              IconButton(onPressed: () => {}, icon: const Icon(Icons.kayaking))
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {},
+        child: const Icon(Icons.brush_rounded),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
   }
 }
