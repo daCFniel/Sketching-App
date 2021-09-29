5a5ba24b690006bf4c99b0fc71693616d6360108
Daniel Bielech <daniel.bielech@ideaworks.co.uk>
2021-09-29 13:09:51 +0100

    Improved overall look of color pickers

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:sketching_app/painter.dart';
 import 'package:sketching_app/point.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:tinycolor2/tinycolor2.dart';
 
 void main() {
   runApp(const MyApp());
@@ -33,78 +35,37 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  // Screen dimensions
   late double _width;
   late double _height;
+
+  // List of points
   final _sketchPoints = <SketchPoint?>[];
+  // List of points for eraser
+  final _erasedPoints = <SketchPoint>[];
+  bool _isEraserMode = false;
 
   // Brush styling
   final double _strokeWidth = 45.0;
   final StrokeCap _strokeCap = StrokeCap.round;
-  Color _currentBrushColor = Colors.amber;
+  Color _currentBrushColor = Colors.deepOrange;
+  Color _brushIconColor = Colors.white;
 
   //UI styling
   final double _bottomMenuHeight = 70.0;
-  Color _currentBackgroundColor = Colors.blue.shade400;
-
-  // currentColor setter
-  void changeColorBrush(Color color) {
-    setState(() {
-      _currentBrushColor = color;
-    });
-  }
-
-  void changeColorBackground(Color color) {
-    setState(() {
-      _currentBackgroundColor = color;
-    });
-  }
-
-  Widget showColorPickerBrush(BuildContext context) {
-    return AlertDialog(
-      title: const Text('Brush color!'),
-      content: SingleChildScrollView(
-          child: ColorPicker(
-        pickerColor: _currentBrushColor,
-        onColorChanged: changeColorBrush,
-        showLabel: true,
-        pickerAreaHeightPercent: 0.8,
-      )),
-      actions: [
-        TextButton(
-            onPressed: () {
-              setState(() {});
-              Navigator.pop(context);
-            },
-            child: const Text('Done'))
-      ],
-    );
-  }
-
-  Widget showColorPickerBackground(BuildContext context) {
-    return AlertDialog(
-      title: const Text('Background coslor!'),
-      content: SingleChildScrollView(
-          child: ColorPicker(
-        pickerColor: _currentBackgroundColor,
-        onColorChanged: changeColorBackground,
-        showLabel: true,
-        pickerAreaHeightPercent: 0.8,
-      )),
-      actions: [
-        TextButton(
-            onPressed: () {
-              setState(() {});
-              Navigator.pop(context);
-            },
-            child: const Text('Done'))
-      ],
-    );
-  }
+  Color _currentBackgroundColor = Color(0xFFABCEEB);
 
   @override
   Widget build(BuildContext context) {
     _width = MediaQuery.of(context).size.width;
     _height = MediaQuery.of(context).size.height;
+    // Change the color of brush icon depending on the current brush color
+    if (TinyColor(_currentBrushColor).getBrightness() > 200) {
+      _brushIconColor = Colors.black;
+    } else {
+      _brushIconColor = Colors.white;
+    }
+
     return Scaffold(
       body: Container(
         color: _currentBackgroundColor,
@@ -113,22 +74,40 @@ class _MainPageState extends State<MainPage> {
         child: GestureDetector(
           onPanStart: (DragStartDetails details) {
             setState(() {
-              _sketchPoints.add(SketchPoint(
-                  details.localPosition,
-                  Paint()
-                    ..strokeWidth = _strokeWidth
-                    ..strokeCap = _strokeCap
-                    ..color = _currentBrushColor));
+              if (_isEraserMode) {
+                _erasedPoints.add(SketchPoint(
+                    details.localPosition,
+                    Paint()
+                      ..strokeWidth = _strokeWidth
+                      ..strokeCap = _strokeCap
+                      ..color = _currentBackgroundColor));
+              } else {
+                _sketchPoints.add(SketchPoint(
+                    details.localPosition,
+                    Paint()
+                      ..strokeWidth = _strokeWidth
+                      ..strokeCap = _strokeCap
+                      ..color = _currentBrushColor));
+              }
             });
           },
           onPanUpdate: (DragUpdateDetails details) {
             setState(() {
-              _sketchPoints.add(SketchPoint(
-                  details.localPosition,
-                  Paint()
-                    ..strokeWidth = _strokeWidth
-                    ..strokeCap = _strokeCap
-                    ..color = _currentBrushColor));
+              if (_isEraserMode) {
+                _erasedPoints.add(SketchPoint(
+                    details.localPosition,
+                    Paint()
+                      ..strokeWidth = _strokeWidth
+                      ..strokeCap = _strokeCap
+                      ..color = _currentBackgroundColor));
+              } else {
+                _sketchPoints.add(SketchPoint(
+                    details.localPosition,
+                    Paint()
+                      ..strokeWidth = _strokeWidth
+                      ..strokeCap = _strokeCap
+                      ..color = _currentBrushColor));
+              }
             });
           },
           onPanEnd: (DragEndDetails details) {
@@ -137,7 +116,7 @@ class _MainPageState extends State<MainPage> {
             });
           },
           child: CustomPaint(
-            painter: MyPainter(_sketchPoints),
+            painter: MyPainter(_sketchPoints, _erasedPoints, _isEraserMode),
           ),
         ),
       ),
@@ -169,8 +148,78 @@ class _MainPageState extends State<MainPage> {
         onPressed: () =>
             showDialog(context: context, builder: showColorPickerBrush),
         child: const Icon(Icons.brush_rounded),
+        backgroundColor: _currentBrushColor,
+        foregroundColor: _brushIconColor,
+        hoverColor: TinyColor(_currentBrushColor).brighten(10).color,
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+
+  // current brush color setter
+  void changeColorBrush(Color color) {
+    setState(() {
+      _currentBrushColor = color;
+    });
+  }
+
+  // current bg color setter
+  void changeColorBackground(Color color) {
+    setState(() {
+      _currentBackgroundColor = color;
+    });
+  }
+
+// brush color picker dialog
+  Widget showColorPickerBrush(BuildContext context) {
+    return AlertDialog(
+      title: Text(
+        'Brush Color!',
+        style: GoogleFonts.pacifico().copyWith(fontSize: 32),
+        textAlign: TextAlign.center,
+      ),
+      content: SingleChildScrollView(
+          child: MaterialPicker(
+        pickerColor: _currentBrushColor,
+        onColorChanged: changeColorBrush,
+      )),
+      actions: [
+        TextButton(
+            onPressed: () {
+              setState(() {});
+              Navigator.pop(context);
+            },
+            child: const Icon(
+              Icons.done_rounded,
+              size: 32,
+            ))
+      ],
+    );
+  }
+
+// bg color picker dialog
+  Widget showColorPickerBackground(BuildContext context) {
+    return AlertDialog(
+      title: Text('Background color!',
+          style: GoogleFonts.pacifico().copyWith(fontSize: 32),
+          textAlign: TextAlign.center),
+      content: SingleChildScrollView(
+          child: ColorPicker(
+        pickerColor: _currentBackgroundColor,
+        onColorChanged: changeColorBackground,
+        pickerAreaHeightPercent: 0.8,
+        enableAlpha: false,
+        showLabel: false,
+        pickerAreaBorderRadius: BorderRadius.circular(10),
+      )),
+      actions: [
+        TextButton(
+            onPressed: () {
+              setState(() {});
+              Navigator.pop(context);
+            },
+            child: const Icon(Icons.done_rounded, size: 32))
+      ],
     );
   }
 }
